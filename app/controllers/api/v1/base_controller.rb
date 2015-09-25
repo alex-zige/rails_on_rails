5d08dbf7b407815b99dcbccbf16baf883f523070
alex-zige <alex.zige@gmail.com>
2015-09-25 15:41:25 +1200

    Add Api base controller

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::BaseController < ApplicationController
+
+  include ErrorHandling
+  before_action :check_deliberate_error
+
+  before_action :authenticate_user_from_token!
+
+  before_action :intercom_update
+
+  skip_before_action :verify_authenticity_token
+
+  def default_serializer_options
+    { root: false }
+  end
+
+  private
+  def check_deliberate_error
+    if params[:raise_error].present?
+      case params[:raise_error].to_i
+      when 404
+        raise Exceptions::NotFound
+      when 403
+        raise Exceptions::Forbidden
+      when 401
+        raise Exceptions::Unauthorized
+      when 500
+        raise Exceptions::InternalServerError
+      when 503
+        raise Exceptions::ServiceUnavailable
+      end
+    end
+  end
+
+  def authenticate_user_from_token!
+    user_token = request.headers['HTTP_TOKEN']
+    user_token ||= params[:authentication_token].presence
+    @current_user = user_token && User.find_by_authentication_token(user_token)
+    raise Exceptions::Unauthorized unless @current_user
+  end
+
+end
