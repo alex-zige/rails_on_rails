690ed57b06c33e5f750ed9cba2982dba0297bc4d
alex-zige <alex.zige@gmail.com>
2015-09-25 14:26:55 +1200

    Add paperclip initializer

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:bucket] = ENV['AWS_BUCKET']
+Paperclip::Attachment.default_options[:s3_host_name] = ENV['AWS_S3_HOST_NAME']
+Paperclip::Attachment.default_options[:s3_credentials] = {:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
