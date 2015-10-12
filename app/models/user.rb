3d112041d17fb2f1f15f3f6a0bea36825aaf9bcb
alex-zige <alex.zige@gmail.com>
2015-10-13 11:11:53 +1300

    Add profile_image and email validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,21 @@ class User < ActiveRecord::Base
 
   before_create :ensure_authentication_token
 
+  has_attached_file :profile_image, :styles => { :thumb => "200x200#" ,:small => "500x500>" }
+  validates_attachment_content_type :profile_image, :content_type => /\Aimage\/.*\Z/
+
+  validates :email, uniqueness: true
+  validates :email, format: { with: /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i, message: 'please provide valid email address', multiline: true}
+
   def ensure_authentication_token
     if authentication_token.blank?
       self.authentication_token = generate_authentication_token
     end
+  end
+
+  def reset_authentication_token
+    self.authentication_token = generate_authentication_token
+    self.save
   end
 
   private
